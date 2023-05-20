8a5f645e16fe4313a76a97faa658bb1a98870481
Erdinch <erdinchorhan02@gmail.com>
2023-05-20 22:44:57 +0300

    changed style to sticky

--- src/components/Shortcuts/Shortcuts.tsx
+++ src/components/Shortcuts/Shortcuts.tsx
@@ -2,14 +2,14 @@ import shortcuts from './data';
 
 function Shortcuts() {
   return (
-    <div className='flex flex-col items-center justify-between'>
+    <div className='flex ml-2 flex-col items-center justify-between sticky left-0 top-0'>
       {shortcuts.map((s) => {
         return (
           <a
             type="button"
             href={s.link}
             key={s.key}
-            className='p-2 w-24 flex flex-col justify-center items-center text-white border border-transparent hover:border-white hover:scale-105 transition-all'
+            className='p-2 w-24 flex flex-col justify-center items-center text-white border border-transparent hover:border-red2 hover:scale-105 transition-all'
           >
             <img src={s.ico} alt={s.name} className='object-contain block h-10 w-10' />
             <p>{s.name}</p>
