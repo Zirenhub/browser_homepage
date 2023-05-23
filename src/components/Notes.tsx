371a1173a4ba94fb3e9f5e785e287f7e612c96f4
Erdinch <erdinchorhan02@gmail.com>
2023-05-23 18:33:54 +0300

    notes clear all func

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid';
 import { useEffect, useRef, useState } from 'react';
 import { Notes, Note } from '../types/notes';
 
@@ -30,54 +30,79 @@ function NotesComp() {
   function handleSubmit() {
     const { title, content } = note;
     if (title && content) {
-      const allNotes = [...notes, {...note, key: uuidv4()}];
+      const allNotes = [...notes, { ...note, key: uuidv4() }];
       setNote({ title: '', content: '' });
       setNotes(allNotes);
       saveNotes(allNotes);
     }
   }
 
+  function handleClear() {
+    localStorage.removeItem('notes');
+    setNotes([]);
+  }
+
   useEffect(() => {
-    const savedNotesJSON = localStorage.getItem('notes')
+    const savedNotesJSON = localStorage.getItem('notes');
     if (savedNotesJSON) {
       const savedNotes: Notes = JSON.parse(savedNotesJSON);
-      setNotes(savedNotes)
+      setNotes(savedNotes);
     }
-  }, [])
+  }, []);
 
   return (
-    <div className="flex flex-col w-fit gap-3 border p-2 rounded-sm border-dim-gray bg-dim-black max-w-[250px]">
-      <input
-        type="text"
-        onChange={handleChange}
-        value={note.title}
-        name="title"
-        placeholder="Title"
-        className={inputClass}
-      />
-      <textarea
-        onChange={handleChange}
-        value={note.content}
-        name="content"
-        placeholder="Content"
-        ref={textAreaRef}
-        className={`${inputClass} overflow-hidden`}
-      />
-      {notes.map((n) => {
-        return (
-          <div key={n.key} className="grow max-h-12 overflow-scroll max-h-20">
-            <p className="text-green2 text-center">{n.title}</p>
-            <p className="text-white text-center break-words">{n.content}</p>
-          </div>
-        );
-      })}
-      <button
-        type="button"
-        onClick={handleSubmit}
-        className="transition-all text-blue2 bg-gray/20 hover:bg-gray/60"
-      >
-        save note
-      </button>
+    <div className="flex flex-col overflow-scroll gap-3 rounded-sm bg-dim-black w-[250px] hover:border-red2/60">
+      <div className="sticky top-0 bg-dim-black w-full flex flex-col justify-center">
+        <p className="text-yellow2 self-center">Notes</p>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={note.title}
+          name="title"
+          placeholder="Title"
+          className={inputClass}
+        />
+        <textarea
+          onChange={handleChange}
+          value={note.content}
+          name="content"
+          placeholder="Content"
+          ref={textAreaRef}
+          className={`${inputClass} overflow-hidden`}
+        />
+      </div>
+      <div className="flex flex-col px-3">
+        {notes.length > 0 ? (
+          notes.map((n) => {
+            return (
+              <div key={n.key} className='border-b border-dim-gray'>
+                <p className="text-green2 text-center">{n.title}</p>
+                <p className="text-white text-center break-words">
+                  {n.content}
+                </p>
+              </div>
+            );
+          })
+        ) : (
+          <p className='text-center text-blue2/40'>Notes go here!</p>
+        )}
+      </div>
+      <div className="sticky bottom-0 p-2 w-full bg-dim-black flex justify-between items-center">
+        <button
+          type="button"
+          onClick={handleSubmit}
+          className="transition-all text-blue2 w-[60%] bg-gray/20 hover:bg-gray/60"
+        >
+          save note
+        </button>
+        <button
+          type="button"
+          onClick={handleClear}
+          className="text-red2 text-sm"
+        >
+          Clear all
+        </button>
+      </div>
     </div>
   );
 }
