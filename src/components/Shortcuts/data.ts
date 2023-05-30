a2555b7d4a19bfb3ca70e3f8beec0538330eae8c
Erdinch <erdinchorhan02@gmail.com>
2023-05-30 23:11:43 +0300

    using uuid

--- src/components/Shortcuts/data.ts
+++ src/components/Shortcuts/data.ts
@@ -3,37 +3,38 @@ import redditIco from '../../assets/reddit.svg';
 import twitchIco from '../../assets/twitch.svg';
 import twitterIco from '../../assets/twitter.svg';
 import windyIco from '../../assets/windy.svg';
+import { v4 as uuidv4 } from 'uuid';
 
 const shortcuts = [
   {
     name: 'Youtube',
     link: 'https://www.youtube.com/',
     ico: youtubeIco,
-    key: 1,
+    _id: uuidv4(),
   },
   {
     name: 'Reddit',
     link: 'https://www.reddit.com/',
     ico: redditIco,
-    key: 2,
+    _id: uuidv4(),
   },
   {
     name: 'Twitch',
     link: 'https://www.twitch.tv/',
     ico: twitchIco,
-    key: 3,
+    _id: uuidv4(),
   },
   {
     name: 'Twitter',
     link: 'https://twitter.com/',
     ico: twitterIco,
-    key: 4,
+    _id: uuidv4(),
   },
   {
     name: 'Windy',
     link: 'https://www.windy.com/',
     ico: windyIco,
-    key: 5,
+    _id: uuidv4(),
   },
 ];
 
