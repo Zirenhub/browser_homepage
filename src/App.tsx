3c9df59b99ecbbf7a3364f9543520f2403689c1b
Erdinch <erdinchorhan02@gmail.com>
2023-05-31 16:52:36 +0300

    added notes, changed design

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import './index.css';
 import Shortcuts from './components/Shortcuts/Shortcuts';
 import Clock from './components/Clock';
-import Notes from './components/Notes';
+import Notes from './components/Notes/Notes';
 import Quote from './components/Quote';
 
 function App() {
   return (
-    <main className="bg-background h-full w-full flex flex-col p-2">
-      <div className="grow">
+    <main className="bg-background h-full w-full flex flex-col p-2 overflow-scroll">
+      <div className="h-full flex flex-col gap-3 items-start overflow-hidden">
         <Clock />
+        <Notes />
       </div>
-      <div className="flex items-end gap-3">
-        <Shortcuts />
+      <div className="flex items-end">
+        <div className="flex flex-col items-start gap-3">
+          <p className="text-4xl font-bold text-aqua2 break-keep">
+            Welcome, <span className="text-red2">Erdinch</span>
+          </p>
+          <Shortcuts />
+        </div>
         <Quote />
       </div>
     </main>
