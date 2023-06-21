ea73b59c44011abfd05f5623e1b9e9c60f3d0284
Erdinch <erdinchorhan02@gmail.com>
2023-06-21 21:21:49 +0300

    redesigned shortcuts

--- src/components/Shortcuts/Shortcuts.tsx
+++ src/components/Shortcuts/Shortcuts.tsx
@@ -1,38 +1,55 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { TShortcut } from '../../types/shortcuts';
 import shortcuts from './data';
 
 function Shortcuts() {
-  const [hoveredLink, setHoveredLink] = useState<string | null>(null);
+  const [formatedShortcuts, setFormatedShortcuts] = useState<TShortcut[][]>([]);
+  const [hoveredName, setHoveredName] = useState<string | null>(null);
+
+  function formatArray(arr: TShortcut[]) {
+    const result = [];
+    for (let i = 0; i < arr.length; i += 3) {
+      result.push(arr.slice(i, i + 3));
+    }
+    return result;
+  }
+
+  useEffect(() => {
+    const formated = formatArray(shortcuts);
+    setFormatedShortcuts(formated);
+  }, []);
 
   return (
-    <div className="flex bg-white shrink-0 rounded-lg shadow-md">
-      {shortcuts.map((s) => {
-        const isHovered = hoveredLink === s._id;
-
+    <div className="flex flex-col relative gap-3 hover:scale-110 transition-all bg-white rounded-t-lg rounded-br-lg shadow-md">
+      {formatedShortcuts.map((x) => {
         return (
-          <a
-            type="button"
-            onMouseEnter={() => setHoveredLink(s._id)}
-            onMouseLeave={() => setHoveredLink(null)}
-            href={s.link}
-            key={s._id}
-            className={`${
-              isHovered ? 'border-b-4 border-aqua' : null
-            } relative flex flex-col items-center justify-center p-2 h-18 w-18`}
-          >
-            {isHovered && (
-              <p className="absolute -top-7 bg-aqua text-white rounded-full px-2">
-                {s.name}
-              </p>
-            )}
-            <img
-              src={s.ico}
-              alt={s.name}
-              className="object-contain block h-8 w-8"
-            />
-          </a>
+          <div className="flex gap-3">
+            {x.map((s) => {
+              return (
+                <a
+                  type="button"
+                  onMouseEnter={() => setHoveredName(s.name)}
+                  onMouseLeave={() => setHoveredName(null)}
+                  href={s.link}
+                  key={s._id}
+                  className="p-2 h-18 w-18 transition-all hover:scale-110"
+                >
+                  <img
+                    src={s.ico}
+                    alt={s.name}
+                    className="object-contain block h-8 w-8"
+                  />
+                </a>
+              );
+            })}
+          </div>
         );
       })}
+      {hoveredName && (
+        <p className="text-yellow font-bold absolute px-3 -bottom-[23.5px] bg-dim-black/50">
+          {hoveredName}
+        </p>
+      )}
     </div>
   );
 }
