34fc20b39981152f8885527e2f1198333d916ec1
Erdinch <erdinchorhan02@gmail.com>
2023-06-21 21:22:00 +0300

    redesigned home

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,26 @@ import Slots from './components/Slots/Slots';
 function App() {
   return (
     <main className="bg-background h-full w-full flex flex-col p-2 overflow-scroll">
-      <div className="h-full flex overflow-hidden">
-        <div className="flex flex-col gap-3">
+      <div className="h-full flex justify-between overflow-hidden relative">
+        <div className="flex flex-col gap-5">
           <Clock />
           <Notes />
         </div>
-        <div className="ml-auto">
+        <div className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
+          <Shortcuts />
+        </div>
+        <div>
           <Slots />
         </div>
       </div>
-      <div className="flex items-end">
-        <Shortcuts />
-        <div className="flex grow justify-between">
-          <div className="flex flex-col justify-end px-4">
-            <p className="text-4xl font-bold text-aqua2 break-keep">
-              Welcome, <span className="text-red2">Erdinch</span>
-            </p>
-            <Quote />
-          </div>
-          <Weather />
+      <div className="flex grow justify-between">
+        <div className="flex flex-col justify-end px-4">
+          <p className="text-4xl font-bold text-aqua2 break-keep">
+            Welcome, <span className="text-red2">Erdinch</span>
+          </p>
+          <Quote />
         </div>
+        <Weather />
       </div>
     </main>
   );
